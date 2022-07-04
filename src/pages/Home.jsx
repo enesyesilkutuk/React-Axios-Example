a27296d7fdecd125cdb0da11adfd7934649b683f
enesyesilkutuk <enesyesilkutuk@gmail.com>
2022-07-04 23:37:29 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,72 @@
+import AddTutorial from '../components/AddTutorial';
+import TutorialList from '../components/TutorialList';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
+  const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+    try {
+      const {data} = await axios.get(url);
+      setTutorials(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => { //! Sadece component mount olduğunda istek yapar
+    getTutorials();
+  }, []);
+
+  //console.log(tutorials);
+
+//! POST (CREATE) 
+  const addTutorial = async (tutorial) => {
+    try {
+      await axios.post(url, tutorial)
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+}
+
+//! DELETE (delete)
+  const deleteTutorial = async (id) => {
+      
+      try {
+      await axios.delete(`${url}/${id}`);
+    
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  }
+
+//! UPDATE (PUT:Whole Update, PATCH:Partially Update)
+
+const editTutorial = async (id, title, desc) => {
+
+  const filtered = tutorials.filter((tutor) => tutor.id === id).map((item) => ({ title: title, description: desc}));
+  
+  // console.log(filtered);
+    
+  try {
+    await axios.put(`${url}/${id}`, filtered[0])
+  } catch (error) {
+    console.log(error);
+  }
+  getTutorials();
+}
+  
+return (
+    <>
+      <AddTutorial addTutorial={addTutorial}/>
+      <TutorialList tutorials={tutorials} deleteTutorial={deleteTutorial} editTutorial={editTutorial}/>
+    </>
+  );
+};
+
+export default Home;
